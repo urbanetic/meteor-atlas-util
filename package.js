150413c05410fd82acda4ea7d993ec3b36e3f9d5
Aram Kocharyan <akarmenia@gmail.com>
2014-11-14 12:20:18 +1100

    Added versioning to dependencies.

--- package.js
+++ package.js
@@ -1,14 +1,15 @@
 Package.describe({
   name: 'urbanetic:atlas-util',
   summary: 'A wrapper with utilities for using Atlas in Meteor.',
-  version: '0.1.3',
+  version: '0.1.4',
   git: 'https://github.com/Urbanetic/meteor-atlas-util.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0');
-  api.use(['coffeescript', 'underscore', 'aramk:requirejs', 'aramk:utility', 'aramk:q',
-    'urbanetic:atlas', 'urbanetic:atlas-cesium'], ['client', 'server']);
+  api.use(['coffeescript', 'underscore', 'aramk:requirejs@2.1.15', 'aramk:utility@0.3.0',
+    'aramk:q@1.0.1', 'urbanetic:atlas@0.7.0-beta', 'urbanetic:atlas-cesium0.7.0-beta'],
+    ['client', 'server']);
   api.use(['deps', 'templating', 'jquery'], 'client');
   api.export([
     'AtlasConverter', 'AtlasManager', 'WKT'
