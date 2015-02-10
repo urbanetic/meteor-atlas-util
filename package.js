4278025fd02747dc2296ae2542bc962adb7379b3
Aram Kocharyan <akarmenia@gmail.com>
2015-02-10 22:11:04 +1100

    Version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'urbanetic:atlas-util',
   summary: 'A wrapper with utilities for using Atlas in Meteor.',
-  version: '0.2.3',
+  version: '0.2.4',
   git: 'https://github.com/Urbanetic/meteor-atlas-util.git'
 });
 
